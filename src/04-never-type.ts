537124465cd77a9506300578699cb61dfb45f328
xMarioOP <marioalejandro424@outlook.com>
2024-10-22 15:12:46 -0500

    Never Type

---
+++ src/04-never-type.ts
@@ -0,0 +1,26 @@
+const withoutEnd = () => {
+  while (true) {
+    console.log(("infinte bucle"));
+
+  }
+}
+
+const fail = (message: string) => {
+  throw new Error(message)
+}
+
+const example = (input: unknown) => {
+  if (typeof input === "string") {
+    return "yes"
+  }else if(Array.isArray(input)){
+    return "Is array"
+  }else{
+    return fail("not mach")
+  }
+}
+
+console.log(example("hi"));
+console.log(example([1,2,3]));
+console.log(example(456));
+console.log(example("hi after fail"));
+
