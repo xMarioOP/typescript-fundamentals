55e35446fd6d5cb1f27867d2a5d7968304beb5f9
xMarioOP <marioalejandro424@outlook.com>
2024-10-22 19:55:42 -0500

    omit and pick type

--- src/app/main.ts
+++ src/app/main.ts
@@ -4,7 +4,6 @@ import { addProduct, products } from './products/product.service';
 
 for (let index = 0; index < 50; index++) {
   addProduct({
-    id: faker.string.uuid(),
     description: faker.commerce.productDescription(),
     image: faker.image.url(),
     color: faker.color.human(),
@@ -13,15 +12,8 @@ for (let index = 0; index < 50; index++) {
     isNew: faker.datatype.boolean(),
     tags: faker.helpers.arrayElements(["idk", "idk2"]),
     title: faker.commerce.productName(),
-    createdAt: faker.date.recent(),
-    updatedAt: faker.date.recent(),
     stock: faker.number.int({ min: 10, max: 100 }),
-    category: {
-      id: faker.string.uuid(),
-      name: faker.commerce.department(),
-      createdAt: faker.date.recent(),
-      updatedAt: faker.date.recent(),
-    }
+    categoryId: faker.string.uuid()
   });
 }
 
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,10 +1,23 @@
+import { faker } from "@faker-js/faker/."
+import { CreateProductDto } from "./product.dto"
 import { Product } from "./product.model"
 
 export const products: Product[] = []
 
 
-export const addProduct = (data: Product) => {
-  // data.id = "asfsafg"               //readonly
-  // data.createdAt = new Date()      //readonly
-  products.push(data)
+export const addProduct = (data: CreateProductDto): Product => {
+  const newProduct = {
+    ...data,
+    id: faker.string.uuid(),
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+    category: {
+      id: data.categoryId,
+      name: faker.commerce.department(),
+      createdAt: faker.date.recent(),
+      updatedAt: faker.date.recent(),
+    }
+  }
+  products.push(newProduct)
+  return newProduct
 }
